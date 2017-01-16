15c6069e7adc97771a6ff5afc2ff739abb16bb87
David Slakter <davidslakter@cpe-2606-a000-1019-408c-2bb6-d7f2-266-d797.dyn6.twc.com>
2017-01-15 19:31:54 -0500

    Added Swap Button, Privacy Policy, swapped, swaps views

--- SwapsViewController.swift
+++ SwapsViewController.swift
@@ -8,28 +8,62 @@
 
 import Foundation
 
-class SwapsViewController: UIViewController, UITableViewDelegate {
+class SwapsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    @IBOutlet var tableView: UITableView!
+    var swapHistoryUsers: [SwapHistory] = []
+    
+    override func viewDidLoad() {
         
         
         SwapUser().getSwapHistory { (error, swapHistory) in
             
             if error != nil{
                 
+                print("error retriving swap history")
             }
             else{
-                
+
+                DispatchQueue.main.async {
+                    
+                    self.swapHistoryUsers = swapHistory!
+                    self.tableView.reloadData()
+                    
+                }
+              
                 
             }
-            
+   
         }
+     
+    }
+    @IBAction func didTapBack(_ sender: Any) {
+        
+        navigationController?.popViewController(animated: true)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return swapHistoryUsers.count
     }
-
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "swapsCell", for: indexPath) as! swapsTableCell;
+        
+        let user = swapHistoryUsers[indexPath.item]
+        
+        cell.username.text = user._swapped
+        cell.swapDate.text = "\(user._time as! Double)"
+        
+        
+        return cell
+    }
+    
+}
+
+class swapsTableCell: UITableViewCell {
+    
+    @IBOutlet var profilePicture: UIImageView!
+    @IBOutlet var swapDate: UILabel!
+    @IBOutlet var username: UILabel!
 }
