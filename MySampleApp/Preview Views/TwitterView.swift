07ab78b0c1404134f3ff1892d87e592d3145b6c5
Micheal <micheal@swapapp.co>
2017-03-24 09:05:19 -0500

    Can now tell if a user is private

--- MySampleApp/Preview Views/TwitterView.swift
+++ MySampleApp/Preview Views/TwitterView.swift
@@ -17,10 +17,30 @@ class TwitterView: TWTRTimelineViewController {
         super.viewDidLoad()
       
         let client = TWTRAPIClient()
-       
+       client.loadUser(withID: twitterUserID ?? "") { (user, error) in
+        
+            user?.isProtected
+        
+        guard error == nil else{
+            print("Error... Could not obtain Twitter. Invalid twitter ID or user does not have twitter connected")
+            
+            return
+        }
+        
+        
+        guard !(user?.isProtected ?? true) else {
+            
+            print("Cannot view Tweets of a protected/private user. Twitter is private")
+            
+            return
+        }
+        
         self.dataSource = TWTRUserTimelineDataSource(screenName: nil, userID: twitterUserID, apiClient: client, maxTweetsPerRequest: 500, includeReplies: true, includeRetweets: true)
         TWTRTweetView.appearance().backgroundColor = UIColor(colorLiteralRed: 0.110, green: 0.161, blue: 0.212, alpha: 1.00)
         TWTRTweetView.appearance().primaryTextColor = UIColor.white
         self.showTweetActions = true
+        
+        }
+        
     }
 }
