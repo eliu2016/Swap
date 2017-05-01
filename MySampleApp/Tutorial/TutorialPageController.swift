b31d61187a8a383e3d151c6104b9bc2a06a604fd
David Slakter <davidslakter@Davids-MacBook-Pro.local>
2017-05-01 11:38:47 -0400

    fixed tutorial

--- MySampleApp/Tutorial/TutorialPageController.swift
+++ MySampleApp/Tutorial/TutorialPageController.swift
@@ -8,15 +8,99 @@
 
 import Foundation
 
-class TutorialPageController: UIPageViewController {
+class TutorialPageController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
     
-    var ViewCotrollers: [UIViewController] = []
+    var tpage1 = grabStoryboard().instantiateViewController(withIdentifier: "tpage1")
+    var tpage2 = grabStoryboard().instantiateViewController(withIdentifier: "tpage2")
+    var tpage3 = grabStoryboard().instantiateViewController(withIdentifier: "tpage3")
+    var tpage4 = grabStoryboard().instantiateViewController(withIdentifier: "tpage4")
+     var tpage5 = grabStoryboard().instantiateViewController(withIdentifier: "tpage5")
+     var tpage6 = grabStoryboard().instantiateViewController(withIdentifier: "tpage6")
+     var tpage7 = grabStoryboard().instantiateViewController(withIdentifier: "tpage7")
+    var tpage8 = grabStoryboard().instantiateViewController(withIdentifier: "tpage8")
+    
+    
+    var tutorialViews = [UIViewController]()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        tutorialViews = [tpage1, tpage2, tpage3, tpage4, tpage5, tpage6, tpage7, tpage8]
+        
+        dataSource = self
+        delegate = self
+        
+        //set the first controller in the pageViewController
+        if let firstViewController = tutorialViews.first {
+            
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+        
+    }
+    
+    /*Delegate function*/
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        
+        //only execute if the transition from one page to the next is completed
+        if completed{
+            
+            //update the current index and the page Control
+           
+            tutorialCurrentIndex = tutorialViews.index(of: (viewControllers?.first)!)!
+            NotificationCenter.default.post(name: .updatePageControl, object: nil)
+        }
+    }
+    
+    /*Data Source Function*/
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = tutorialViews.index(of: viewController) else {
+            
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            
+            return nil
+        }
+        
+        guard tutorialViews.count > previousIndex else {
+            
+            return nil
+        }
+        
+        
+        return tutorialViews[previousIndex]
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        
+        guard let viewControllerIndex = tutorialViews.index(of: viewController) else {
+            
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let pictureViewsCount = tutorialViews.count
+        
+        guard pictureViewsCount != nextIndex else{
+            
+            return nil
+        }
+        
+        guard pictureViewsCount > nextIndex else{
+            return nil
+        }
+        
+        return tutorialViews[nextIndex]
+        
+    }
     
 
-    
-    
-    //setViewControllers(ViewCotrollers, direction: .forward, animated: true, completion: nil)
-    
-    
 
 }
