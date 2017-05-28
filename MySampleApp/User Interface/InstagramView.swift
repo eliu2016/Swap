edaa48016a59b48665abe44da5edc978eb9738f9
David Slakter <davidslakter@Davids-MacBook-Pro.local>
2017-05-27 20:09:18 -0400

    fixed preview bugs for all screens

--- MySampleApp/User Interface/InstagramView.swift
+++ MySampleApp/User Interface/InstagramView.swift
@@ -14,6 +14,7 @@ class instagramView: UIViewController, UITableViewDelegate, UITableViewDataSourc
     
 
     @IBOutlet var activityView: UIActivityIndicatorView!
+
     
     @IBOutlet var tableView: UITableView!
     
